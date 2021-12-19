636e13e593f7c60f570482cfe131606392ed16c5
AutoBoy <87021395+AutoBoyWinner@users.noreply.github.com>
2021-12-19 09:50:59 -0800

    change mongoDB

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/sowfee',
+  // mongoURI: 'mongodb://localhost:27017/sowfee',
+  mongoURI: 'mongodb+srv://isam:9vBt55JETeo2MBoS@sowfee.bj4mk.mongodb.net/sowfee?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
